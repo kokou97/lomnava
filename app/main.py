c172ef76fe4ba854deba0f2562bb1a28015d1d89
kokou97 <jdjadou@yahoo.fr>
2022-03-22 23:32:13 +0100

    rearranger routers order

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
-app.include_router(auth.router)
 app.include_router(vote.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
