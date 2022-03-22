2786d75fd6815fe5ce2aa1c56ee846d306ee5f7d
kokou97 <jdjadou@yahoo.fr>
2022-03-22 12:17:56 +0100

    useOAuthPasswordRequestForm for user credentials

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 # SECRET_KEY
 # Algorithm
 # Expiration time
-
+# openssl rand -hex 32 in ubuntu terminal
 SECRET_KEY = "e764fa8d2f62a445c26b07372ba5737c4b2a1ab68da4f453e70a8ed36c595709"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 from typing_extensions import dataclass_transform
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
 
@@ -7,9 +8,11 @@ router = APIRouter(tags=['Authentification'])
 
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+
+    # OAuth2PasswordRequestForm only return username and password
     user = db.query(models.User).filter(
-        models.User.email == user_credentials.email).first()
+        models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(
