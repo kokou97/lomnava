11a824172efd1060504d1378f0d007f1484aa2b9
kokou97 <jdjadou@yahoo.fr>
2022-03-22 17:20:41 +0100

    user can only retrieve posts he owns

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,7 +14,8 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(
+        models.Post.owner_id == current_user.id).all()
     return posts
 
 
@@ -39,6 +40,10 @@ def get_post(id: int, db: Session = Depends(get_db), current_user=Depends(oauth2
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id : {id} was not found")
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
     return post
 
 
