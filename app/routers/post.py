66c2ede68fb373311e8d2b7b538410b5442c311e
kokou97 <jdjadou@yahoo.fr>
2022-03-22 17:22:40 +0100

    rolling back the previous commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,8 +14,7 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).filter(
-        models.Post.owner_id == current_user.id).all()
+    posts = db.query(models.Post).all()
     return posts
 
 
@@ -41,9 +40,6 @@ def get_post(id: int, db: Session = Depends(get_db), current_user=Depends(oauth2
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id : {id} was not found")
 
-    if post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Not authorized to perform requested action")
     return post
 
 
